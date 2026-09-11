41ba100fbc5d4e78ae9a1a7b792633570b12cad5
Larissa <larissaqramos1@gmail.com>
2026-09-11 15:38:34 -0300

    feat: Created UserServices and implemented getUsers and getUsersById and fixed UserResources

--- src/main/java/com/loyius/course/resources/UserResources.java
+++ src/main/java/com/loyius/course/resources/UserResources.java
@@ -1,17 +1,41 @@
 package com.loyius.course.resources;
 
 import com.loyius.course.entities.User;
+import com.loyius.course.services.UserServices;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
+
+    private final UserServices userServices;
+
+    public UserResources(UserServices userServices) {
+        this.userServices = userServices;
+    }
+
     @GetMapping
-    public ResponseEntity<User> getUser() {
-        User user = new User(1L, "Maria", "maria@gmail.com", "99999", "1234");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> getUser() {
+        List<User> list = userServices.getUsers();
+        if (list.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long id) {
+        User obj = userServices.getUserById(id);
+        if (obj == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/loyius/course/services/UserServices.java
@@ -0,0 +1,25 @@
+package com.loyius.course.services;
+
+import com.loyius.course.entities.User;
+import com.loyius.course.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServices {
+
+    private final UserRepository repository;
+    public UserServices(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> getUsers() {
+        return repository.findAll();
+    }
+    public User getUserById(Long id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElse(null);
+    }
+}
