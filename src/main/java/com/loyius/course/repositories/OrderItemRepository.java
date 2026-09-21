4a8d173c37757875c73155cdbe5967b9b57c41ff
Larissa <larissaqramos1@gmail.com>
2026-09-21 16:58:30 -0300

    fix: Implemented right parameter (in the right file now....)

--- src/main/java/com/loyius/course/repositories/OrderItemRepository.java
+++ src/main/java/com/loyius/course/repositories/OrderItemRepository.java
@@ -1,7 +1,8 @@
 package com.loyius.course.repositories;
 
 import com.loyius.course.entities.OrderItem;
+import com.loyius.course.entities.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
 }
--- src/main/java/com/loyius/course/repositories/OrderRepository.java
+++ src/main/java/com/loyius/course/repositories/OrderRepository.java
@@ -1,9 +1,8 @@
 package com.loyius.course.repositories;
 
 import com.loyius.course.entities.Order;
-import com.loyius.course.entities.pk.OrderItemPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<Order, OrderItemPK> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
 }
