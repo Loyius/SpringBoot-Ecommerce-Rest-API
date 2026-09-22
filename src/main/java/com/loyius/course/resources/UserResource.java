a5453b63439d0168eafc79a7dc41e63630866dbe
Larissa <larissaqramos1@gmail.com>
2026-09-22 18:51:15 -0300

    feat: Added the UPDATE and DELETE method to User

--- src/main/java/com/loyius/course/resources/UserResource.java
+++ src/main/java/com/loyius/course/resources/UserResource.java
@@ -3,9 +3,11 @@ package com.loyius.course.resources;
 import com.loyius.course.entities.User;
 import com.loyius.course.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +34,7 @@ public class UserResource {
     }
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long id) {
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
         User obj = service.getUserById(id);
         if (obj == null) return ResponseEntity.notFound().build();
         return ResponseEntity.ok().body(obj);
@@ -45,4 +47,17 @@ public class UserResource {
         //created() expects a header(location) with the URI to the added content
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id) {
+        service.deleteUserById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> updateUserById(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.updateUserById(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
--- src/main/java/com/loyius/course/services/UserService.java
+++ src/main/java/com/loyius/course/services/UserService.java
@@ -20,9 +20,26 @@ public class UserService {
     }
 
     public User getUserById(Long id) {
+        //gets the object directly from the db
         Optional<User> user = repository.findById(id);
         return user.orElse(null);
     }
 
     public User insertNewUser(User obj){ return repository.save(obj); }
+
+    public void deleteUserById(Long id){ repository.deleteById(id); }
+
+    public User updateUserById(Long id, User obj){
+        //Prepare the monitored object to update and then post it in the db
+        User entity  = repository.getReferenceById(id);
+        updateData(entity,obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPhone(obj.getPhone());
+    }
+
 }
