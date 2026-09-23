8b5d60005aa7a89bab4e69250644bc1a9154a506
Larissa <larissaqramos1@gmail.com>
2026-09-22 22:24:18 -0300

    feat: Created exception treatment for "DatabaseException"

--- src/main/java/com/loyius/course/services/UserService.java
+++ src/main/java/com/loyius/course/services/UserService.java
@@ -2,7 +2,10 @@ package com.loyius.course.services;
 
 import com.loyius.course.entities.User;
 import com.loyius.course.repositories.UserRepository;
+import com.loyius.course.services.exceptions.DatabaseException;
 import com.loyius.course.services.exceptions.ResourceNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,7 +31,16 @@ public class UserService {
 
     public User insertNewUser(User user){ return repository.save(user); }
 
-    public void deleteUserById(Long id){ repository.deleteById(id); }
+    public void deleteUserById(Long id){
+        try{
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e){
+            //for me to find the type of exception being thrown we can use:
+            //catch(RuntimeException e){ e.printStackTrace();}
+            throw new ResourceNotFoundException(id);
+        } catch(DataIntegrityViolationException e){ throw new DatabaseException(e.getMessage());
+        }
+    }
 
     public User updateUserById(Long id, User user){
         //Prepare the monitored object to update and then post it in the db
