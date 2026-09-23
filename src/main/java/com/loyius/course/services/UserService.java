14a0f8176bda5980851409492c04a9002d91da6d
Larissa <larissaqramos1@gmail.com>
2026-09-22 22:13:44 -0300

    feat: Created exception treatment for "ResourceNotFound"

--- src/main/java/com/loyius/course/services/UserService.java
+++ src/main/java/com/loyius/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.loyius.course.services;
 
 import com.loyius.course.entities.User;
 import com.loyius.course.repositories.UserRepository;
+import com.loyius.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,24 +23,24 @@ public class UserService {
     public User getUserById(Long id) {
         //gets the object directly from the db
         Optional<User> user = repository.findById(id);
-        return user.orElse(null);
+        return user.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
-    public User insertNewUser(User obj){ return repository.save(obj); }
+    public User insertNewUser(User user){ return repository.save(user); }
 
     public void deleteUserById(Long id){ repository.deleteById(id); }
 
-    public User updateUserById(Long id, User obj){
+    public User updateUserById(Long id, User user){
         //Prepare the monitored object to update and then post it in the db
         User entity  = repository.getReferenceById(id);
-        updateData(entity,obj);
+        updateData(entity,user);
         return repository.save(entity);
     }
 
-    private void updateData(User entity, User obj) {
-        entity.setName(obj.getName());
-        entity.setEmail(obj.getEmail());
-        entity.setPhone(obj.getPhone());
+    private void updateData(User entity, User user) {
+        entity.setName(user.getName());
+        entity.setEmail(user.getEmail());
+        entity.setPhone(user.getPhone());
     }
 
 }
